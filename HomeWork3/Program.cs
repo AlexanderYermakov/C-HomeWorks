b088216b2ef48127a93405a5030f52e865e7066e
Alejandro Yermakov <cakkoc@yandex.ru>
2023-07-29 06:22:51 +0500

    task 2 added

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,1 +1,69 @@
-﻿
+﻿//Домашнее задание к семинару №3
+
+/* Задача 19
+Напишите программу, которая принимает на вход число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+
+bool CheckingNumber(int number)
+{
+    
+
+}
+
+
+Console.ReadLine()
+*/
+
+
+Console.Write("Please input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// 3113
+int dig = num / 10;
+
+while((dig % 10) == 0)
+{
+    int n1 = num % 10;
+    
+}
+
+//int n1 = num % 10;
+
+Console.WriteLine(dig);
+Console.WriteLine(n1);
+
+
+
+/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+
+
+/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+void CubeNumber(int n)
+{
+    int number = 1;
+    while(number <= n)
+    {
+        Console.WriteLine($"{number} - {Math.Pow(number, 3)}");
+        number++;
+    }
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num <= 0) Console.WriteLine("Uncorrect! Please input correct number");
+
+CubeNumber(num);
+*/
