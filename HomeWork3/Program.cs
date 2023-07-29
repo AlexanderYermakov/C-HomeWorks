5a77dd76168e3e163c0d687b7c851611a16d0e95
Alejandro Yermakov <cakkoc@yandex.ru>
2023-07-30 00:14:16 +0500

    task 2 comlete

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -15,7 +15,7 @@ bool CheckingNumber(int number)
 
 
 Console.ReadLine()
-*/
+
 
 
 Console.Write("Please input a number: ");
@@ -34,7 +34,7 @@ while((dig % 10) == 0)
 
 Console.WriteLine(dig);
 Console.WriteLine(n1);
-
+*/
 
 
 /* Задача 21
@@ -43,7 +43,27 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
+double Distance3D(double xA, double yA, double zA, double xB, double yB, double zB)
+{
+    double count = (xB - xA) * (xB - xA) + (yB - yA) * (yB - yA) + (zB - zA) * (zB - zA);
+    return Math.Round(Math.Sqrt(count), 2);
+}
 
+Console.Write("Please enter the X coordinate for point A: ");
+double xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the Y coordinate for point A: ");
+double yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the Z coordinate for point A: ");
+double zA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the X coordinate for point B: ");
+double xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the Y coordinate for point B: ");
+double yB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the Z coordinate for point B: ");
+double zB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Distance3D(xA, yA, zA, xB, yB, zB));
 
 /* Задача 23
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
