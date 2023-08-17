2774fd697de0a5ed671222902d06f31f7c56fb7d
Alejandro Yermakov <cakkoc@yandex.ru>
2023-08-17 23:32:54 +0500

    home work 5 complete

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -3,8 +3,9 @@
 /*
 Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
+*/
 
-int[] CreateArray(int size)
+int[] CreateArray(int size)     // Создание массива
 {
     int[] array = new int[size];
     for(int i = 0; i < size; i++)
@@ -13,7 +14,7 @@ int[] CreateArray(int size)
     return array;
 }
 
-void WriteArray(int[] array)
+void WriteArray(int[] array)    // Вывод заполненного массива на экран (для информации)
 {
     for(int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
@@ -21,7 +22,7 @@ void WriteArray(int[] array)
     Console.WriteLine();
 }
 
-int Counting(int[] array)
+int Counting(int[] array)       // Подсчет количества четных элементов
 {
     int quantity = 0;
     for(int i = 0; i < array.Length; i++)
@@ -40,14 +41,15 @@ int count = Counting(myArray);
 WriteArray(myArray);
 
 Console.WriteLine($"Количество четных чисел: {count} ");
-*/
+
 
 /*
 Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
-int[] CreateArray(int size, int minValue, int maxValue)
+
+int[] CreateArray(int size, int minValue, int maxValue)     // Создание массива
 {
     int[] array = new int[size];
     for(int i = 0; i < size; i++)
@@ -56,7 +58,7 @@ int[] CreateArray(int size, int minValue, int maxValue)
     return array;
 }
 
-void WriteArray(int[] array)
+void WriteArray(int[] array)        // Вывод заполненного массива на экран (для информации)
 {
     for(int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
@@ -64,7 +66,7 @@ void WriteArray(int[] array)
     Console.WriteLine();
 }
 
-int Checking(int[] array)
+int Counting(int[] array)       // Подсчет суммы нечетных элементов
 {
     int sum = 0;
     for(int i = 1; i < array.Length; i += 2)
@@ -84,13 +86,61 @@ int maxValue = Convert.ToInt32(Console.ReadLine());
 
 int[] myArray = CreateArray(size, minValue, maxValue);
 
-int sum = Checking(myArray);
+int sum = Counting(myArray);
 
 WriteArray(myArray);
 
 Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях в массиве: {sum} ");
 
+
+
+
 /*
 Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 */
+
+double[] CreateArray(int size)  // Заполнение массива
+{
+    double[] array = new double[size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i} элемент массива: ");
+        array[i] = Convert.ToDouble(Console.ReadLine());
+    }  
+    return array;
+}
+
+void WriteArray(double[] array)     // Вывод заполненного массива на экран (для информации)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+        
+    Console.WriteLine();
+}
+
+double DifferenceNumbers(double[] array)       // Определение минимального и максимального эл-ов, подсчет разницы
+{
+    double min = array[0];
+    double max = array[0];
+    double result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > max) max = array[i];
+        if(array[i] < min) min = array[i];
+    }
+    result = max - min;
+    return result;
+}
+
+Console.WriteLine("Задайте длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+double[] myArray = CreateArray(size);
+
+double result =  DifferenceNumbers(myArray);
+
+WriteArray(myArray);
+
+Console.WriteLine($"Разница между минимальным и максимальным элементами = {result}");
+
